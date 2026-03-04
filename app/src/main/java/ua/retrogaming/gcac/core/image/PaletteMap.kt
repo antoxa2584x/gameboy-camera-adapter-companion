29f82d5ec42f8d60e59569c452d56b06ae9da9ba
Anton Artemov <antoxa2584@gmail.com>
2026-03-04 20:36:40 +0200

    Updated upscaler to save all details

--- app/src/main/java/ua/retrogaming/gcac/core/image/PaletteMap.kt
+++ app/src/main/java/ua/retrogaming/gcac/core/image/PaletteMap.kt
@@ -3,7 +3,7 @@ package ua.retrogaming.gcac.core.image
 typealias PaletteMap = Map<Int, Int>
 
 data class Thresholds(
-    val t1: Int = 210,
-    val t2: Int = 150,
-    val t3: Int = 90
+    val t1: Int = 200,
+    val t2: Int = 120,
+    val t3: Int = 40
 )
